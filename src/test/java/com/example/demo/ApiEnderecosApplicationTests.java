01a8e6b6976250428fbca86bd16355eb1f01e13c
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-12-13 13:51:35 -0300

    consultar enderecos test, excluir enderecos test

--- src/test/java/com/example/demo/ApiEnderecosApplicationTests.java
+++ src/test/java/com/example/demo/ApiEnderecosApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -22,6 +25,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.demo.domain.models.dtos.EnderecoRequestDto;
 import com.example.demo.domain.models.dtos.EnderecoResponseDto;
+import com.example.demo.domain.models.entities.Endereco;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 
@@ -94,5 +99,51 @@ class ApiEnderecosApplicationTests {
 
 		assertTrue(content.contains("Endereço atualizado com sucesso!"));
 	}
+
+	@Test
+	@Order(3)
+	void consultarEnderecoPorIdTest() throws Exception {
+
+		var result = mockMvc.perform(get("/api/enderecos/" + id)).andExpect(status().isOk()).andReturn();
+
+		var content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+		var response = objectMapper.readValue(content, Endereco.class);
+
+		assertEquals(response.getId(), id);
+		assertNotNull(response.getLogradouro());
+		assertNotNull(response.getNumero());
+		assertNotNull(response.getComplemento());
+		assertNotNull(response.getBairro());
+		assertNotNull(response.getCidade());
+		assertNotNull(response.getUf());
+		assertNotNull(response.getCep());
+	}
+
+	@Test
+	@Order(4)
+	void consultarEnderecosTest() throws Exception {
+
+		var result = mockMvc.perform(get("/api/enderecos")).andExpect(status().isOk()).andReturn();
+
+		var content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+		var response = objectMapper.readValue(content, new TypeReference<List<Endereco>>() {
+		});
+
+		response.stream().filter(produto -> produto.getId().equals(id)).findFirst().orElseThrow();
+	}
+	
+	@Test
+	@Order(5)
+	void excluirEnderecoTest() throws Exception {
+		
+		var result = mockMvc.perform(delete("/api/enderecos/" + id)).andExpect(status().isOk()).andReturn();
+		
+		var content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+		
+		assertTrue(content.contains("Endereço excluído com sucesso!"));
+	}
+	
 
 }
